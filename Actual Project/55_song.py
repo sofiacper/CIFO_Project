5519f426aa67bfdfb21664e9e18749d8da2d72cc
sofiacper <144894124+sofiacper@users.noreply.github.com>
2024-05-12 11:34:33 +0100

    Changes

--- Actual Project/55_song.py
+++ Actual Project/55_song.py
@@ -1,6 +1,7 @@
 from charles.charles import Population, Individual
 from charles.search import hill_climb, sim_annealing
 from copy import copy
+from math import sqrt
 from charles.selection import fps, tournament_sel
 from charles.mutation import swap_mutation
 from charles.xo import cycle_xo
@@ -13,11 +14,10 @@ def get_fitness(self):
         int: the total distance of the path
     """
     fitness = 0
-    target = [60, 60 ,62 ,64, 64, 62, 60, 59, 59, 60, 60, 59, 59, 60, 60, 62, 64, 64, 62, 60, 59, 59, 60, 59, 60, 59, 59, 59,59, 60, 59, 60, 62, 60, 59, 60, 59, 60, 62, 64, 62, 60, 60, 62, 64, 64, 62, 60, 59, 59, 60, 59,60, 59, 59]
+    target = [60,60,62,64,64,62,60,59,59,60,60,59,59,60,60,62,64,64,62,60,59,59,60,59, 60, 59, 59, 59,59, 60, 59, 60, 62, 60, 59, 60, 59, 60, 62, 64, 62, 60, 60, 62, 64, 64, 62, 60, 59, 59, 60, 59,60, 59, 59]
     for i in range(len(self.representation)):
-        # using normalized difference between values
-        fitness += ((target[i] - self.representation[i])+127)/(127+127)
-    return fitness
+        fitness += ((target[i] - self.representation[i]))**2
+    return sqrt(fitness)
 
 
 def get_neighbours(self):
@@ -43,9 +43,7 @@ Individual.get_neighbours = get_neighbours
 P = Population(size=40, optim="min", sol_size=55,
                  valid_set=[i for i in range(127)], repetition = True)
 
-#P.evolve(gens=100, xo_prob=0.9, mut_prob=0.15, select=tournament_sel,xo=cycle_xo, mutate=swap_mutation, elitism=True)
+P.evolve(gens=100, xo_prob=0.9, mut_prob=0.15, select=tournament_sel,xo=cycle_xo, mutate=swap_mutation, elitism=True)
 
-
-
-hill_climb(P)
+#hill_climb(P)
 #sim_annealing(P)
